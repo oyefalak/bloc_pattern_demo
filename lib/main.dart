26227fef69e09b0a2b001778da3be20340709f4c
Shabbir Rajput <sabbirrajput03@gmail.com>
2024-11-26 16:08:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:bloc_pattern_demo/framework/blocs/counter_bloc/counter_bloc.dart';
 import 'package:bloc_pattern_demo/framework/blocs/sign_in_bloc/sign_in_bloc.dart';
-import 'package:bloc_pattern_demo/framework/cubits/counter_cubit/counter_cubit.dart';
-import 'package:bloc_pattern_demo/ui/counter_page/counter_page.dart';
 import 'package:bloc_pattern_demo/ui/sign_in_page/sign_in_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
